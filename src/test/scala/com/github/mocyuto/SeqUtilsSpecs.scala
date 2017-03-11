851152f8e3e6b9704fd17179d2c53cbeb227be77
Yuto Suzuki <yuutoo.advance@gmail.com>
2017-03-11 19:44:43 +0900

    add emptyOrElse Test

--- src/test/scala/com/github/mocyuto/SeqUtilsSpecs.scala
+++ src/test/scala/com/github/mocyuto/SeqUtilsSpecs.scala
@@ -5,13 +5,26 @@ import com.github.mocyuto.SeqUtils._
 
 class SeqUtilsSpecs extends FlatSpec with Matchers {
 
+  "Non empty Seq" should "return by oneself" in {
+    assert(Seq(1).emptyOrElse(Seq(0)) == Seq(1))
+  }
+
   it should "not throw Exception" in {
     Seq(1).emptyOrElse(throw new Exception) shouldBe Seq(1)
   }
 
-  it should "throw Exception" in {
-    val ex = new Exception
-    val thrown = the[Exception] thrownBy (Nil.emptyOrElse(throw ex))
-    thrown shouldBe ex
+  "Empty Seq" should "return argument" in {
+    assert(Nil.emptyOrElse(Seq(0)) == Seq(0))
   }
+
+  it should "throw Exception" in {
+    assertThrows[Exception] {
+      Nil.emptyOrElse(throw new Exception)
+    }
+  }
+
+  "Non empty List" should "return by oneself" in {
+    assert(List(1).emptyOrElse(List(0)) == List(1))
+  }
+
 }
